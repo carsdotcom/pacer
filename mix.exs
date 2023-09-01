85f16b09ce5bc2bc39e7f10aa273a833ac18243c
Zack Kayser <zkayser@gmail.com>
2023-09-01 11:03:53 -0400

    Update mix project definition

--- mix.exs
+++ mix.exs
@@ -1,17 +1,24 @@
 defmodule Pacer.MixProject do
   use Mix.Project
 
+  @name "Pacer"
+  @version "0.1.0"
+  @source_url "https://github.com/carsdotcom/pacer"
+
   def project do
     [
       app: :pacer,
-      version: "0.1.0",
+      contributors: contributors(),
+      name: @name,
+      version: @version,
+      source_url: @source_url,
       build_path: "../../_build",
-      config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
+      description: description(),
+      package: package(),
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
-      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
@@ -25,8 +32,28 @@ defmodule Pacer.MixProject do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
+  def contributors() do
+    [
+      {"Zack Kayser", "@zkayser"},
+      {"Stephanie Lane", "@stelane"}
+    ]
+  end
+
+  defp description do
+    "Dependency graphs for optimal function call ordering"
+  end
+
+  defp package do
+    [
+      name: "pacer",
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
   defp deps do
     [
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:libgraph, "~> 0.16.0"},
       {:nimble_options, ">= 0.0.0"},
       {:telemetry, "~> 0.4"}
