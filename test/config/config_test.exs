00bb80fd2cbc00b61abbb736bb2bc200af36206f
Zack Kayser <zkayser@gmail.com>
2023-10-14 14:36:16 -0400

    Add test case for when only global batch_telemetry_options are provided

--- test/config/config_test.exs
+++ test/config/config_test.exs
@@ -4,6 +4,16 @@ defmodule Pacer.ConfigTest do
   alias Pacer.Config
 
   describe "batch_telemetry_options/1" do
+    setup do
+      default = Application.get_env(:pacer, :batch_telemetry_options)
+
+      on_exit(fn ->
+        Application.put_env(:pacer, :batch_telemetry_options, default)
+      end)
+
+      :ok
+    end
+
     defmodule NoOptions do
       use Pacer.Workflow
 
@@ -14,6 +24,11 @@ defmodule Pacer.ConfigTest do
 
     test "returns an empty map if no user-provided options are available" do
       assert Config.batch_telemetry_options(NoOptions) == %{}
+    end
+
+    test "returns global batch_telemetry_options if no module-level options are provided" do
+      Application.put_env(:pacer, :batch_telemetry_options, %{foo: "bar"})
+      assert Config.batch_telemetry_options(NoOptions) == %{foo: "bar"}
     end
   end
 end
