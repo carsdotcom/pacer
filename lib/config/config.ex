bd8890c6b44027ddc3148500500feb73f64aa8d8
Zack Kayser <zkayser@gmail.com>
2023-10-14 14:32:52 -0400

    Add implementation for Config.batch_telemetry_options/1

--- lib/config/config.ex
+++ lib/config/config.ex
@@ -4,4 +4,22 @@ defmodule Pacer.Config do
   extracting user-provided configuration values specific
   to Pacer.
   """
+
+  @spec batch_telemetry_options(module()) :: map()
+  def batch_telemetry_options(workflow_module) do
+    case :persistent_term.get({__MODULE__, workflow_module, :batch_telemetry_options}, :unset) do
+      :unset -> fetch_and_write({workflow_module, :batch_telemetry_options})
+      config -> config
+    end
+  end
+
+  @spec fetch_and_write({workflow_module, key}) :: map() when key: atom(), workflow_module: module()
+  defp fetch_and_write({workflow_module, key}) do
+    global_config = Application.get_env(:pacer, key) || %{}
+    module_config = workflow_module.__config__(key) || %{}
+
+    global_config
+    |> Map.merge(module_config)
+    |> tap(&:persistent_term.put({__MODULE__, workflow_module, key}, &1))
+  end
 end
