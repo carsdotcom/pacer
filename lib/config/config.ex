fb46f29499f29987362f3911406a00ef6c321854
Zack Kayser <zkayser@gmail.com>
2023-10-19 07:42:34 -0400

    Rework batch_telemetry_options configuration to allow for mfa-style args

--- lib/config/config.ex
+++ lib/config/config.ex
@@ -5,7 +5,7 @@ defmodule Pacer.Config do
   to Pacer.
   """
 
-  @spec batch_telemetry_options(module()) :: map()
+  @spec batch_telemetry_options(module()) :: keyword() | {module(), atom(), list()}
   def batch_telemetry_options(workflow_module) do
     case :persistent_term.get({__MODULE__, workflow_module, :batch_telemetry_options}, :unset) do
       :unset -> fetch_and_write({workflow_module, :batch_telemetry_options})
@@ -13,13 +13,35 @@ defmodule Pacer.Config do
     end
   end
 
-  @spec fetch_and_write({workflow_module, key}) :: map() when key: atom(), workflow_module: module()
-  defp fetch_and_write({workflow_module, key}) do
-    global_config = Application.get_env(:pacer, key) || %{}
-    module_config = workflow_module.__config__(key) || %{}
+  @spec fetch_batch_telemetry_options(module()) :: map()
+  def fetch_batch_telemetry_options(workflow_module) do
+    case batch_telemetry_options(workflow_module) do
+      {mod, fun, args} -> Map.new(apply(mod, fun, args))
+      opts -> Map.new(opts)
+    end
+  end
 
-    global_config
-    |> Map.merge(module_config)
-    |> tap(&:persistent_term.put({__MODULE__, workflow_module, key}, &1))
+  @spec fetch_and_write({workflow_module, :batch_telemetry_options}) ::
+          keyword() | {module(), atom(), list()}
+        when workflow_module: module()
+  defp fetch_and_write({workflow_module, :batch_telemetry_options = key}) do
+    global_config = Application.get_env(:pacer, key) || []
+    module_config = workflow_module.__config__(key) || []
+
+    cond do
+      is_list(global_config) && is_list(module_config) ->
+        global_config
+        |> Keyword.merge(module_config)
+        |> tap(&:persistent_term.put({__MODULE__, workflow_module, key}, &1))
+
+      match?({_m, _f, _a}, module_config) || is_list(module_config) ->
+        tap(module_config, &:persistent_term.put({__MODULE__, workflow_module, key}, &1))
+
+      match?({_m, _f, _a}, global_config) || is_list(global_config) ->
+        tap(global_config, &:persistent_term.put({__MODULE__, workflow_module, key}, &1))
+
+      true ->
+        tap([], &:persistent_term.put({__MODULE__, workflow_module, key}, &1))
+    end
   end
 end
